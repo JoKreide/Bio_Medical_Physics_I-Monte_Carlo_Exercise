67e4131ed1ea534959109bcc322bd4fc4397624f
ChalkyChalkson <35282814+ChalkyChalkson@users.noreply.github.com>
2022-01-29 23:20:29 +0100

    Added visualization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import Photons
+import Renderer
 from CDFLookup2D import CDFLookup2D
 from src.CrossSectionLookup import CrossSectionLookup
 import time
@@ -12,36 +13,12 @@ if __name__ == '__main__':
     compton_look_up = CDFLookup2D.from_csv('lookups/compton angles.csv')
     rayleigh_look_up = CDFLookup.from_csv('lookups/rayleigh angles.csv')
 
-    photon_number = 10 ** 6
-    df = Photons.initiate_pencil_beam(0.1, photon_number)
-
-    start = time.time()
-    depositions = Photons.calculate_depositions(df, cross_section_lookup, compton_look_up, rayleigh_look_up, -5, 5, 0, 25)
-    end = time.time()
-
-    print(f"\ndone in: {(end - start) * 10 ** 6 / len(depositions)} s / million events")
-    print()
-    print(f"average y-depth: {np.mean(depositions['y_pos'])}")
-    print(f"max y-depth: {np.max(depositions['y_pos'])}")
-    print(f"stab y-depth: {np.std(depositions['y_pos'])}")
-    print(f"average x: {np.mean(depositions['x_pos'])}")
-    print(f"max x: {np.max(np.abs(depositions['x_pos']))}")
-    print(f"stab x: {np.std(depositions['x_pos'])}")
-
-    print("\n---- FINAL DEPOSITIONS----\n")
-    print(depositions)
-
-    xs = depositions['x_pos']
-    xs.values.sort()
-    plt.figure("Transverse Distribution")
-    plt.plot(xs)
-
-    ys = depositions['y_pos']
-    ys.values.sort()
-    plt.figure("Longitudinal Distribution")
-    plt.plot(ys)
-
-    plt.show()
+    for i in range(5):
+        photon_number = 10 ** 6
+        df = Photons.initiate_pencil_beam(0.1, photon_number)
+        depositions = Photons.calculate_depositions(df, cross_section_lookup, compton_look_up, rayleigh_look_up, -2.5, 2.5, 0, 10)
+        absorbed_dose = np.sum(depositions['energy'].values) / photon_number
+        print(absorbed_dose)
 
 
 
---
+++ src/Renderer.py
@@ -0,0 +1,48 @@
+import numpy as np
+import Photons
+from CDFLookup import CDFLookup
+from CDFLookup2D import CDFLookup2D
+from CrossSectionLookup import CrossSectionLookup
+import matplotlib.pyplot as plt
+
+
+def draw_photon_paths(box_position, box_dimensions, interaction_df, indices, title=''):
+    fig = plt.figure(title)
+    water_rect = plt.Rectangle(box_position, *box_dimensions, facecolor = 'cornflowerblue')
+    plt.gca().add_patch(water_rect)
+
+    for index in indices:
+        interactions = interaction_df[interaction_df['id'] == index]
+        positions = np.array([interactions['x_pos'], interactions['y_pos']]).T
+        positions = np.vstack(([[0, 0]], positions))
+        scaled_energy = interactions['energy'].values
+        scaled_energy = scaled_energy.flatten()
+        scaled_energy = np.log(scaled_energy)
+        scaled_energy = scaled_energy / np.max(scaled_energy)
+        scaled_energy = scaled_energy / 20 * min(box_dimensions)
+        scaled_energy = np.insert(scaled_energy,0,0)
+
+        for pos, energy in zip(positions, scaled_energy):
+            circle = plt.Circle(pos, radius = energy, fc = 'r')
+            plt.gca().add_patch(circle)
+
+    for index in indices:
+        interactions = interaction_df[interaction_df['id'] == index]
+        positions = np.array([interactions['x_pos'], interactions['y_pos']]).T
+        positions = np.vstack(([[0,0]],positions))
+        line = plt.Polygon(positions, closed = None, fill = None, linewidth = 1.5)
+        plt.gca().add_patch(line)
+
+    plt.axis('scaled')
+    return fig
+
+if __name__ == '__main__':
+    cross_section_lookup = CrossSectionLookup.from_csv('../lookups/scattering crossections water.csv', has_keys = True)
+    compton_look_up = CDFLookup2D.from_csv('../lookups/compton angles.csv')
+    rayleigh_look_up = CDFLookup.from_csv('../lookups/rayleigh angles.csv')
+
+    photon_number = 10 ** 3
+    df = Photons.initiate_pencil_beam(0.1, photon_number)
+    depositions = Photons.calculate_depositions(df, cross_section_lookup, compton_look_up, rayleigh_look_up, -2.5, 2.5, 0, 10)
+    draw_photon_paths((-2.5, 0), (5, 10), depositions, depositions['id'][:5])
+    plt.show()
